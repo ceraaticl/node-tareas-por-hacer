48912ea3a355a09073c9bc8419008970e23044f5
Jose Santamaria <santamaria0323@gmail.com>
2021-02-14 19:32:14 -0300

    Repetir el menú de consola

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 
 const main = async () => {
-    mostrarMenu();
-    // pausa();
+    let opt = '';
+    do {
+        opt = await mostrarMenu();
+        await pausa();
+    } while (opt !== '0');
 };
 
 main();

30c7bfdba2282da2999960b8109ad30552e511e4
Jose Santamaria <santamaria0323@gmail.com>
2021-02-15 18:49:14 -0300

    Menú interactivo - carga y lectura de datos JSON

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
-
-console.clear();
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const {
+    inquirerMenu,
+    inquirerPausa,
+    leerInput,
+} = require('./helpers/inquirer');
+const Tareas = require('./models/Tareas');
 
 const main = async () => {
     let opt = '';
+    const tareas = new Tareas();
+    const tareasDB = leerDB();
+    if (tareasDB) {
+        tareas.cargarTareasFromArray(tareasDB);
+    }
     do {
-        opt = await mostrarMenu();
-        await pausa();
+        // opt = await mostrarMenu();
+        // await pausa();
+
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                const desc = await leerInput();
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas.listadoArr);
+                break;
+        }
+        guardarDB(tareas.listadoArr);
+        await inquirerPausa();
     } while (opt !== '0');
 };
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,86 @@
+const inquirer = require('inquirer');
+const { validate } = require('uuid');
+require('colors');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
+            {
+                value: '1',
+                name: `${'1.'.green} Crear tarea`,
+            },
+            {
+                value: '2',
+                name: `${'2.'.green} Listar tareas`,
+            },
+            {
+                value: '3',
+                name: `${'3.'.green} Listar tareas completadas`,
+            },
+            {
+                value: '4',
+                name: `${'4.'.green} Listar tareas pendietes`,
+            },
+            {
+                value: '5',
+                name: `${'5.'.green} Completar tarea(s)`,
+            },
+            {
+                value: '6',
+                name: `${'6.'.green} Borrar tarea`,
+            },
+            {
+                value: '0',
+                name: `${'0.'.green} Salir`,
+            },
+        ],
+    },
+];
+
+const inquirerMenu = async () => {
+    console.clear();
+    console.log('==========================='.green);
+    console.log('   Seleccione una opción   '.green);
+    console.log('===========================\n'.green);
+
+    const { opcion } = await inquirer.prompt(preguntas);
+
+    return opcion;
+};
+
+const inquirerPausa = async () => {
+    const confirmacion = [
+        {
+            type: 'input',
+            name: 'pausa',
+            message: `Presione ${'ENTER'.green} para continuar`,
+        },
+    ];
+    console.log('\n');
+
+    await inquirer.prompt(confirmacion);
+};
+
+const leerInput = async () => {
+    const entrada = [
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Descripción: ',
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor ingrese una descripción';
+                }
+                return true;
+            },
+        },
+    ];
+
+    const { desc } = await inquirer.prompt(entrada);
+    return desc;
+};
+
+module.exports = { inquirerMenu, inquirerPausa, leerInput };
